166c7078c60b2a733719ef42235da6044f307a6a
zusin <42296359+ExplorAction@users.noreply.github.com>
2019-03-14 11:07:16 +0800

    update for mysql.especially take it as package

--- mysql_try.py
+++ mysql_try.py
@@ -1,36 +1,42 @@
-# import MySQLdb
-#
-# class operatemysql:
-#     #获取链接
-#     def get_com(self):
-#         try:
-#             self.con = MySQLdb.connect(
-#                 host='localhost',
-#                 port=3306,
-#                 user="root",
-#                 password="123",
-#                 db="news",
-#                 charset='utf8')
-#             cursor = self.con.cursor()
-#         except MySQLdb.Error as e:
-#             print("Error %d: %s" % (e.args[0],e.args[1]))
-#         return self.con
-#
-# def main():
-#
-# if "__name__" == "main":
-#     main()
 import MySQLdb
 
-conn = MySQLdb.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    db="school",
-    port=3306,
-    charset='utf8'
-)
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM `news`where `id`=13;')
-rest = cursor.fetchone()
-print(rest)
+class MysqlSearch(object):
+    #初始化
+    def __init__(self):
+        self.get_conn()
+    #获取链接
+    def get_conn(self):
+        try:
+            self.conn = MySQLdb.connect(
+                host='localhost',
+                user="root",
+                password="",
+                db="school",
+                port=3306,
+                charset='utf8'
+            )
+        except MySQLdb.Error as e:
+            print("Error %d: %s" % (e.args[0],e.args[1]))
+    #关闭链接
+    def close_conn(self):
+        try:
+            if self.conn:
+                self.conn.close()
+        except MySQLdb.Error as e:
+            print("Error:" %e)
+    #获取一条数据
+    def get_one(self):
+        sql = 'select * from `news` where `id`= 13;'    #%d to avoid
+        cursor = self.conn.cursor()
+        cursor.execute(sql,)
+        rest = cursor.fetchone()
+        print(rest)
+        cursor.close()
+        self.close_conn()
+
+def main():
+    obj = MysqlSearch()
+    obj.get_one()
+
+if __name__ == "__main__":
+    main()
